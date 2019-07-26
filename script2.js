a9ba063185e50b170c5ca10e7ef9d49e2aa43ac4
nenafay <nena.park@gmail.com>
2019-07-26 17:58:04 -0400

    solved dropdown problem. need to work on js for responsive dropdown button

--- script2.js
+++ script2.js
@@ -1,4 +1,13 @@
-function navFunction(x) {
+function navbtnFunction(x) {
     x.classList.toggle("change");
+}
+
+function respNavFunction(){
+    var y = document.getElementById("myTopNav");
+    if(y.className === "topNav") {
+        y.className += "responsive";
+    } else {
+        y.className = "topNav";
+    }
 }
 
